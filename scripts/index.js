f3dd4226446c3e4932228255cfb13efcc16a5336
AlexandrLebedenko <sasha.lebedenko.2012@gmail.com>
2025-08-28 16:28:57 +0300

    feat: add theme saving in LS

--- scripts/index.js
+++ scripts/index.js
@@ -4,17 +4,17 @@ const hamburgerMenuIcon = document.querySelector(".hamburger-menu__icon");
 const navItems = document.querySelectorAll(".nav-list__item");
 const navBox = document.querySelector(".nav-box");
 const themeSlider = document.querySelector(".theme-slider");
-
-let darkTheme = "false";
+const themeSliderIcons = document.querySelectorAll(".icons__item");
+let darkTheme = false;
 
 // Add active class for header links
 navItems.forEach((element) => {
   element.addEventListener("click", () => {
-    // Delete the active class for everyone
+    // Delete the active class for everyone elements
     navItems.forEach((el) => {
       el.classList.remove("nav-list__item--active");
     });
-    // Add the active class to the current
+    // Add the active class to the current element
     element.classList.add("nav-list__item--active");
   });
 });
@@ -36,23 +36,33 @@ function themeContentChange() {
     logoImg.src = "./img/logo-dark.svg";
     moonIcon.src = "./img/Moon_fill.svg";
     sunIcon.src = "./img/Sun_fill_dark.svg";
+    moonIcon.classList.add("icons__item--active");
   } else {
     logoImg.src = "./img/logo-light.svg";
     moonIcon.src = "./img/Moon_fill_light.svg";
     sunIcon.src = "./img/Sun_fill.svg";
+    sunIcon.classList.add("icons__item--active");
   }
 }
-
-themeSlider.addEventListener("click", () => {});
-
 // Theme slider action
 themeSlider.addEventListener("click", () => {
+  darkTheme = !darkTheme;
   themeSliderIcons.forEach((element) => {
-    // Delete the active class for everyone
-
-    themeSliderIcons.src = navBox.classList.contains("icons__item--active") ? "./img/close-button.svg" : "./img/hamburger-button.svg";
-
-    // Add the active class to the current
-    // element.classList.add("icons__item--active");
+    // Add and remove the active class for elements
+    element.classList.toggle("icons__item--active");
   });
+  themeContentChange();
+  saveThemeToLocalStorage();
 });
+// Load theme from localStorage
+function loadThemeFromLocalStorage() {
+  const savedTheme = localStorage.getItem("darkTheme");
+  darkTheme = savedTheme === "true";
+  themeContentChange();
+}
+// Save theme to localStorage
+function saveThemeToLocalStorage() {
+  localStorage.setItem("darkTheme", darkTheme.toString());
+}
+
+loadThemeFromLocalStorage();
